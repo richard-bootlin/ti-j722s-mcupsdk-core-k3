9205feb70b0f3cc1317521cbbd1de68a83858230
Richard Genoud (TI.com) <richard.genoud@bootlin.com>
2026-01-26 16:09:04 +0100

    dbg: add more functions

--- source/dbg_uart.c
+++ source/dbg_uart.c
@@ -1,24 +1,28 @@
 
 #define WKUP_UART0_BASE 0x2b300000U
+#define MAIN_UART0_BASE 0x28000000U
 #define MCU_UART0_BASE  0x04a00000U
 #define DBG_UART_LCR 0x0CU
 #define DBG_UART_LSR 0x14U
 #define DBG_UART_THR 0x00U
 
-#define UART_LSR_TX_SR_E_MASK 0x40U
-#define UART_LSR_TX_FIFO_E_MASK 0x20U
+#define DBG_UART_LSR_TX_SR_E_MASK 0x40U
+#define DBG_UART_LSR_TX_FIFO_E_MASK 0x20U
+#define __maybe_unused __attribute__((__unused__))
+
+static const unsigned int do_wait = 1;
 
 static inline void dbg_writel(unsigned int addr, unsigned int val)
 {
-	*(volatile unsigned int *) (addr) = val;
+    *(volatile unsigned int *) (addr) = val;
 }
 
 static inline unsigned int dbg_readl(unsigned int addr)
 {
-	return *(volatile unsigned int *) (addr);
+    return *(volatile unsigned int *) (addr);
 }
 
-static void _dgb_putc(unsigned int base_addr, unsigned char c)
+static void _dbg_putc(unsigned int base_addr, unsigned char c)
 {
     unsigned int lcrRegValue = dbg_readl(base_addr + DBG_UART_LCR);
 
@@ -28,11 +32,11 @@ static void _dgb_putc(unsigned int base_addr, unsigned char c)
      * Waits indefinitely until the THR and Transmitter Shift Registers are
      * empty.
      */
-    while(((unsigned int) UART_LSR_TX_SR_E_MASK |
-           (unsigned int) UART_LSR_TX_FIFO_E_MASK) !=
+    while(do_wait && (((unsigned int) DBG_UART_LSR_TX_SR_E_MASK |
+           (unsigned int) DBG_UART_LSR_TX_FIFO_E_MASK) !=
           (dbg_readl(base_addr + DBG_UART_LSR) &
-           ((unsigned int) UART_LSR_TX_SR_E_MASK |
-            (unsigned int) UART_LSR_TX_FIFO_E_MASK)))
+           ((unsigned int) DBG_UART_LSR_TX_SR_E_MASK |
+            (unsigned int) DBG_UART_LSR_TX_FIFO_E_MASK))))
     {
         /* Busy wait */
     }
@@ -41,16 +45,32 @@ static void _dgb_putc(unsigned int base_addr, unsigned char c)
     dbg_writel(base_addr + DBG_UART_LCR, lcrRegValue);
 }
 
-static void dgb_putc(unsigned char c)
+static void dbg_putc(unsigned char c)
 {
     unsigned int base = WKUP_UART0_BASE;
 
     static int configured;
+    configured = 0;
     if (!configured)
     {
         dbg_writel(base + DBG_UART_LCR, 0x3 /* 8N1 */);
-	configured = 1;
+        configured = 1;
     }
 
-    _dgb_putc(base, c);
+    _dbg_putc(base, c);
 }
+
+static void dbg_puts(const char *str)
+{
+    for (; *str != '\0'; str++)
+    {
+        dbg_putc(*str);
+    }
+}
+
+__maybe_unused static void dbg_line(const char *str)
+{
+    dbg_puts(str);
+    dbg_putc('\n');
+}
+
