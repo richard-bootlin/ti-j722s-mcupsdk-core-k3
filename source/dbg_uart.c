1158d6a8223ead9d15fb517a887bc023efeb5317
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-01-27 15:28:05 +0100

    debug: add dump reg

--- source/dbg_uart.c
+++ source/dbg_uart.c
@@ -75,25 +75,43 @@ __maybe_unused static void dbg_line(const char *str)
     dbg_putc('\n');
 }
 
-__maybe_unused static void dump_HEX(void *addr, unsigned int size)
+static void dump_byte(unsigned char byte)
 {
     const char *const hex = "0123456789abcdef";
+
+    if (byte < 0x10) {
+        dbg_putc('0');
+        dbg_putc(hex[byte]);
+    } else {
+        dbg_putc(hex[(byte >> 4) & 0xf]);
+        dbg_putc(hex[byte & 0xf]);
+    }
+}
+
+__maybe_unused static void dump_HEX(void *addr, unsigned int size)
+{
     unsigned char *c = addr;
 
     for (unsigned int i = 1; i <= size; i++)
     {
-        if (*c < 0x10) {
-            dbg_putc('0');
-            dbg_putc(hex[*c]);
-        } else {
-            dbg_putc(hex[(*c >> 4) & 0xf]);
-            dbg_putc(hex[*c & 0xf]);
-        }
+        dump_byte(*c);
         if ((i % 16) == 0)
             dbg_puts("\r\n");
         else
             dbg_putc(' ');
         c++;
-
     }
+}
+
+__maybe_unused static void dump_reg(char *name, unsigned int addr)
+{
+    unsigned int val = dbg_readl(addr);
+
+    dbg_puts(name);
+    dbg_puts(": ");
+    dump_byte((val & 0xff000000) >> 24);
+    dump_byte((val & 0xff0000) >> 16);
+    dump_byte((val & 0xff00) >> 8);
+    dump_byte(val & 0xff);
+    dbg_puts("\r\n");
 }
