c2d21de8754dcb95f7346880c5107add546fc538
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-02-18 08:28:06 +0100

    select easily the test ram pattern activation

--- source/drivers/device_manager/s2r/lpm_s2r.c
+++ source/drivers/device_manager/s2r/lpm_s2r.c
@@ -1220,12 +1220,15 @@ if (1) {
 void Lpm_enterRetention(void)
 {
 	dbg_line("Lpm_enterRetention: Enter retention");
+#define DO_RAM_PATTERN_TEST 0
 #define RAM_START 0x80000000U
 #define SZ 2048U
 
-for (unsigned int i = 0; i < SZ; i++) {
-	writel(i, RAM_START + 4 * i);
-}
+	if (DO_RAM_PATTERN_TEST) {
+		for (unsigned int i = 0; i < SZ; i++) {
+			writel(i, RAM_START + 4 * i);
+		}
+	}
 	/* Make sure that nothing remains in cache before going to retention */
 	Lpm_cleanAllDCache();
 
@@ -1241,7 +1244,9 @@ for (unsigned int i = 0; i < SZ; i++) {
 ddr_exit_low_power_mode();
 //	Lpm_setupPmic();
 
-dump_HEX((void*)RAM_START, SZ*4);
+	if (DO_RAM_PATTERN_TEST) {
+		dump_HEX((void*)RAM_START, SZ*4);
+	}
 	while(1){};
 }
 
